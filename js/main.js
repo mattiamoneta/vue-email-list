4d4ddfacdb3f6ceb0a2d4eff34fc9eb19296270b
Mattia Moneta <moneta.mattia@yahoo.com>
2023-03-23 15:56:26 +0100

    Final

---
+++ js/main.js
@@ -0,0 +1,25 @@
+ const { createApp } = Vue
+
+  createApp({
+    data() {
+      return {
+        arrayEmails : []
+      }
+    },
+    methods: {
+       getEmails(){
+        for(let i = 0; i < 10; i++){
+            const newMail = axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((e) => {
+                this.arrayEmails.push(e.data.response);
+            });
+        }
+       }
+       
+    },
+
+    mounted(){
+        this.getEmails();
+    }
+
+  }).mount('#app')
